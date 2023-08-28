ab2eaa77f1e51429eb582dcc9837d13db17e991c
Ronald Allan <115074375+ronaldpaguntalan@users.noreply.github.com>
2023-08-28 11:45:28 +0800

    00:01:42 Angular & Angular CLI 00:02:12 What is Angular? 00:07:21 What is Single Page Application? 00:09:27 What is Angular CLI? 00:10:34 Angular CLI Setup 00:12:30 First Angular App 00:15:21 Angular File Structure Part I 00:20:07 Angular File Structure Part II 00:21:46 Angular File Structure Part III 00:24:57 End Section Angular & Angular CLI 00:25:33 Intro Angular Components 00:26:06 What is Angular Component 00:31:27 Create an Angular Component 00:40:51 How Loads Components Inside the Browser 00:44:24 Templates & Styles Component Properties 00:49:42 Better Approach for Templates & Styles 00:55:17 Generate Angular Components using Angular CLI 01:00:06 NgOnInit() Lifecycle Hook 01:02:11 Data Binding 01:05:43 Sharing Data Between Components 01:07:55 Parent Child Relationship 01:11:04 @Input Decorator 01:15:27 Task 01:16:51 Solution 01:21:17 @ViewChild Decorator 01:29:11 @Output & Event Emitter 01:37:47 End Section Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ChangeDetectorRef} from '@angular/core';
+import { PostComponent } from './post/post.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements AfterViewInit {
   title = 'AngIntro';
+  parentMessage:string = 'Message change';
+  message:string | undefined;
+  fromChildOutput:string | undefined;
+
+  @ViewChild(PostComponent)
+  childComp!: PostComponent; 
+
+  constructor(private cdRef: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    this.message = this.childComp.childMessage;
+    this.cdRef.detectChanges(); // Manually trigger change detection
+    console.log("Message from child:", this.message);
+  }
+
+  receiveMessage($event: string){
+    this.fromChildOutput = $event;
+  }
 }
